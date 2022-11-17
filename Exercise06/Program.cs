05010864439b35032b93750daca2c0ee4d129d5b
Mariia <mari.gontareva@inbox.ru>
2022-11-18 04:34:19 +0700

    Exercise06 added

---
+++ Exercise06/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число.");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 1)
+{
+    Console.WriteLine("Введённое число является чётным.");
+
+}
+else
+{
+    Console.WriteLine("Введённое число не является чётным.");
+}
+break;
+  
