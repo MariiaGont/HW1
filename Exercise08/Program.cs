476080d83b9daf1e21eb5ed75b7cbfac7811a6dd
Mariia <mari.gontareva@inbox.ru>
2022-11-18 04:34:39 +0700

    Exercise08 added

---
+++ Exercise08/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите положительное число.");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+while (number > 0)
+{
+    if (count < number)
+    {
+        Console.Write(count + " ");
+        count = count + 2;
+    }
+}
+if (number < 0)
+{
+    Console.WriteLine("Введённое число не является положительным.");
+}
+
