48e79c60eca7ee59c6ea6dd52418746d5ca9aa02
Mariia <mari.gontareva@inbox.ru>
2022-11-18 04:33:55 +0700

    Exercise02 added

---
+++ Exercise02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("На вход принимается два числа и выдаётся, какое число большее, а какое меньшее.");
+Console.WriteLine("Введите первое число.");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число.");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+
+if (numberFirst > numberSecond)
+{
+    Console.WriteLine(numberFirst);
+}
+else
+{
+    Console.WriteLine(numberSecond);
+}
