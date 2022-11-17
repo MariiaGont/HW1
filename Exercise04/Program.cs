cd75240c5c01478a4366b77a54700601819668cc
Mariia <mari.gontareva@inbox.ru>
2022-11-18 04:34:07 +0700

    Exercise04 added

---
+++ Exercise04/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("На вход принимается три числа и выдаётся максимальное из этих чисел.");
+Console.WriteLine("Введите первое число.");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число.");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число.");
+int numberThird = Convert.ToInt32(Console.ReadLine());
+
+int max = numberFirst;
+
+ if (numberSecond > max)
+{
+    max = numberSecond;
+}
+
+if (numberThird > max)
+{
+    max = numberThird;
+}
+
+Console.WriteLine($"max = {max}");
